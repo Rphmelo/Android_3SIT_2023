9533c93a1c5b7818ef92703d1b9c27ea51a64654
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-04-03 22:45:54 -0300

    Aula 7 - Projeto ChatGpt parte 2

--- ChatGPT/app/src/main/java/br/com/fiap/chatgpt/ChatActivity.kt
+++ ChatGPT/app/src/main/java/br/com/fiap/chatgpt/ChatActivity.kt
@@ -1,5 +1,6 @@
 package br.com.fiap.chatgpt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.fiap.chatgpt.databinding.ActivityChatBinding
@@ -12,5 +13,36 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupButtons()
+        binding.textInputEditTextMessage.setText(
+            intent.extras?.getString(Intent.EXTRA_TEXT)
+        )
     }
+
+    private fun setupButtons() {
+        binding.buttonSendMessage.setOnClickListener {
+            binding.messageValue.text = binding.textInputEditTextMessage.text
+            clearText()
+        }
+
+        binding.buttonShareMessage.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, binding.messageValue.text.toString())
+            }
+
+            val shareIntent = Intent.createChooser(
+                sendIntent,
+                getString(R.string.chooser_title)
+            )
+
+            startActivity(shareIntent)
+        }
+    }
+
+    private fun clearText() {
+        binding.textInputEditTextMessage.text?.clear()
+    }
+
 }
