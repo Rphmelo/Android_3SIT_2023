1ddcbecf3f2fe49f7c0e6f74d166ae3c0aac37c6
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-04-10 22:43:41 -0300

    Aula 8 Recycler view parte 1

--- ChatGPT/app/src/main/java/br/com/fiap/chatgpt/presentation/QuestionsActivity.kt
+++ ChatGPT/app/src/main/java/br/com/fiap/chatgpt/presentation/QuestionsActivity.kt
@@ -3,7 +3,9 @@ package br.com.fiap.chatgpt.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.fiap.chatgpt.data.TalkDataSource
 import br.com.fiap.chatgpt.databinding.ActivityQuestionsBinding
+import br.com.fiap.chatgpt.presentation.adapter.QuestionAdapter
 
 class QuestionsActivity : AppCompatActivity() {
 
@@ -13,6 +15,13 @@ class QuestionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityQuestionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerViewQuestions.adapter = QuestionAdapter(
+            TalkDataSource.talkList
+        )
     }
 
     private fun goToAnswersActivity() {
---
+++ ChatGPT/app/src/main/java/br/com/fiap/chatgpt/presentation/adapter/QuestionAdapter.kt
@@ -0,0 +1,41 @@
+package br.com.fiap.chatgpt.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.fiap.chatgpt.R
+import br.com.fiap.chatgpt.data.TalkModel
+import br.com.fiap.chatgpt.databinding.ViewQuestionItemBinding
+
+class QuestionAdapter(
+    private val talkList: List<TalkModel>
+) :
+    RecyclerView.Adapter<QuestionAdapter.QuestionViewHolder>() {
+
+    inner class QuestionViewHolder(
+        itemView: View
+    ): RecyclerView.ViewHolder(itemView) {
+        private val binding = ViewQuestionItemBinding.bind(itemView)
+
+        fun bind(item: TalkModel) {
+            binding.questionTitle.text = item.question
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionViewHolder {
+        return QuestionViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.view_question_item, parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) {
+        holder.bind(talkList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return talkList.size
+    }
+}
