0c5d3737847b777f5e18de88ca3b9e88254222f3
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-04-24 22:35:58 -0300

    Aula 10 - Projeto Countries parte 1

---
+++ Countries/app/src/main/java/br/com/fiap/countries/RegisterCountryFragment.kt
@@ -0,0 +1,27 @@
+package br.com.fiap.countries
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.fiap.countries.databinding.FragmentRegisterCountryBinding
+
+class RegisterCountryFragment : Fragment() {
+
+    lateinit var binding: FragmentRegisterCountryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterCountryBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+}
