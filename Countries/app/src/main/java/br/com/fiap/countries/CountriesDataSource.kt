d9b73e49cce6215f091a902ef06a6c6b28a4a934
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-05-08 22:52:26 -0300

    Aula 11 - Projeto Countries Parte 2

---
+++ Countries/app/src/main/java/br/com/fiap/countries/CountriesDataSource.kt
@@ -0,0 +1,9 @@
+package br.com.fiap.countries
+
+import br.com.fiap.countries.model.CountryModel
+
+object CountriesDataSource {
+
+    val countriesList: MutableList<CountryModel> = mutableListOf()
+
+}
--- Countries/app/src/main/java/br/com/fiap/countries/CountriesFragment.kt
+++ Countries/app/src/main/java/br/com/fiap/countries/CountriesFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import br.com.fiap.countries.databinding.FragmentCountriesBinding
 
 class CountriesFragment : Fragment() {
 
     lateinit var binding: FragmentCountriesBinding
+
+    private val countryAdapter = CountryAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,5 +25,23 @@ class CountriesFragment : Fragment() {
             false
         )
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        binding.buttonAddCountry.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_to_RegisterCountryFragment
+            )
+        }
+
+        binding.recyclerViewCountries.setHasFixedSize(true)
+        binding.recyclerViewCountries.adapter = countryAdapter
+
+        countryAdapter.setData(CountriesDataSource.countriesList)
     }
 }
